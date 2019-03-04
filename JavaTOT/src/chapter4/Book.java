d23c472a102ccad8ff12c0f3dde2967bcaa43aba
zainimar <zainimm@gmail.com>
2019-03-04 16:45:07 +0800

    first commit

--- JavaTOT/src/chapter4/Book.java
+++ JavaTOT/src/chapter4/Book.java
@@ -9,10 +9,15 @@ public class Book { //step 1
      static int bil = 0; //keyword static - shared by all and own by class ex; acuan yang boleh kira jumlah dibuat bukan baulu
      String title;
      
+     // dia own by class bukan own by object
      public static void cetak(){ //step 4 - this is a method sbb ada public - static digunakan di method
          System.out.println("Jumlah buku = " + Book.bil);
          
      }
+     
+     /*public void info(){ //step 6
+         
+     }*/
      
      Book(){ // step 3 
          Book.bil = Book.bil + 1;
@@ -22,6 +27,7 @@ public class Book { //step 1
      public static void main(String[] args){ //step 2
          Book b1 = new Book(); //b1 dan b2 - adalah object
          Book b2 = new Book();
-         Book.cetak(); // step 5 
+         Book.cetak(); // boleh jg buat cetak(); step 5 - ambil di method cetak - function static boleh panggil function static jg
+         //info(); step 7 - akan error daripada step 6
      }
 }
